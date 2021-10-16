ac16bb73fc68a96c68b5598ead2fb3028ea7d3c5
Florian Suess <floriansuess96@icloud.com>
2021-10-16 21:00:52 +1300

    Cleanup flags for client schema generation

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"context"
 	_ "embed"
 	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
 	"time"
 )
@@ -16,14 +19,15 @@ import (
 //go:embed introspectQuery.graphql
 var introspectSchema string
 
-func main() {
-	ctx := context.Background()
-	endpoint := os.Getenv("SERVER_ENDPOINT")
-	if strings.TrimSpace(endpoint) == "" {
-		log.Fatal("SERVER_ENDPOINT must be provided")
-	}
+const DEFAULT_ENDPOINT = "http://localhost:8080/query"
 
-	authorization := os.Getenv("AUTHORIZATION_HEADER")
+func main() {
+	var endpoint string
+	headers := make(headers)
+
+	flag.StringVar(&endpoint, "endpoint", DEFAULT_ENDPOINT, "GraphQL server endpoint")
+	flag.Var(&headers, "header", "Headers to be passed endpoint (can appear multiple times)")
+	flag.Parse()
 
 	buffer := new(bytes.Buffer)
 	err := json.NewEncoder(buffer).Encode(graphQLRequest{Query: introspectSchema})
@@ -31,15 +35,18 @@ func main() {
 		log.Fatal(err)
 	}
 
+	client := http.Client{Timeout: 2 * time.Minute}
 	req, err := http.NewRequest(http.MethodPost, endpoint, buffer)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	req.Header.Add("Authorization", authorization)
+	req.Header = http.Header(headers)
 	req.Header.Add("Content-Type", "application/json")
 
-	client := http.Client{Timeout: 2 * time.Minute}
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
 	res, err := client.Do(req.WithContext(ctx))
 	if err != nil {
 		log.Fatal(err)
@@ -54,9 +61,29 @@ func main() {
 
 	if len(schemaResponse.Errors) != 0 {
 		log.Fatal(schemaResponse.Errors)
+    return
 	}
 
 	fmt.Println(printSchema(schemaResponse.Data.Schema))
+}
+
+type headers map[string][]string
+
+func (h headers) Set(input string) error {
+	values := strings.Split(input, "=")
+	if len(values) < 2 {
+		return errors.New(`header must appear like 'Authorization="Bearer token"'`)
+	}
+	h[values[0]] = append(h[values[0]], values[1:]...)
+	return nil
+}
+
+func (h *headers) String() string {
+	sb := strings.Builder{}
+	for header, values := range *h {
+		sb.WriteString(fmt.Sprintf("%s=%s", header, strings.Join(values, ",")))
+	}
+	return sb.String()
 }
 
 type graphQLRequest struct {
